ec2bdd0fa1241be4eb5ec70c1098011556b815d9
Zion Pulapakura <kingofbeastz777@gmail.com>
2025-07-06 21:28:50 +1000

    made basic file structure in client

---
+++ client/src/components/LoadDocumentBtn.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@mui/material";
+
+const LoadDocumentBtn = () => {
+  return (
+    <Button variant="contained" color="primary">
+      Load Legal Document
+    </Button>
+  );
+};
+
+export default LoadDocumentBtn;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import LoadDocumentBtn from "../components/LoadDocumentBtn";
+import QueryTextField from "../components/QueryTextField";
+
+function Home() {
+  return (
+    <div>
+      <LoadDocumentBtn />
+      <QueryTextField />
+      
+      <p>info here</p>
+    </div>
+  );
+}
+
+export default Home;
